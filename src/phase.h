d30d6fcc096384ab6f3c0bf5d76dc521ec028bcf
LarryG <101243518+GitLarryG@users.noreply.github.com>
2023-05-09 23:15:27 +0000

    Question 4

--- src/phase.h
+++ src/phase.h
@@ -1,1 +1,38 @@
-//
+//create an abstract Phase class that will be the base class for derived classes ComeOutPhase and PointPhase.
+
+//In phase.h file create an enum class RollOutcome with values natural, craps, point, seven_out, and nopoint
+
+#include <iostream>
+#include <random>
+#include <ctime>
+#include "die.h"
+#include "roll.h"
+#include "shooter.h"
+
+
+#ifndef PHASE_H
+#define PHASE_H
+
+class RollOutcome {
+    public:
+        enum class Value {natural, craps, point, seven_out, nopoint};
+        RollOutcome(Value v) : value(v) {}
+        Value get_value() const {return value;}
+    private:
+        Value value;
+};
+
+class ComeOutPhase {
+    public:
+        RollOutcome get_outcome(Roll* roll);
+};
+
+class PointPhase {
+    public:
+        PointPhase(int p) : point(p) {}
+        RollOutcome get_outcome(Roll* roll, int point_value);
+    private:
+        int point;
+};
+
+#endif
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -3,6 +3,7 @@
 #include "die.h"
 #include "roll.h"
 #include "shooter.h"
+#include "phase.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -35,4 +36,50 @@ TEST_CASE("Test Shooter class shoot function")
 	Roll* roll = shooter.shoot(die1, die2);
 	REQUIRE(roll->roll_value() >= 2);
 	REQUIRE(roll->roll_value() <= 12);
+}
+
+TEST_CASE("Test ComeOutPhase get_outcome function returns natural, craps, or point")
+{
+	Die die1;
+	Die die2;
+	Shooter shooter;
+	Roll* roll = shooter.shoot(die1, die2);
+	ComeOutPhase come_out_phase;
+	RollOutcome roll_outcome = come_out_phase.get_outcome(roll);
+	if (roll_outcome.get_value() == RollOutcome::Value::natural)
+	{
+		REQUIRE(roll_outcome.get_value() == RollOutcome::Value::natural);
+	}
+	else if (roll_outcome.get_value() == RollOutcome::Value::craps)
+	{
+		REQUIRE(roll_outcome.get_value() == RollOutcome::Value::craps);
+	}
+	else
+	{
+		REQUIRE(roll_outcome.get_value() == RollOutcome::Value::point);
+	}
+}
+
+TEST_CASE("Test PointPhase get_outcome function returns point, seven_out, or nopoint")
+{
+	Die die1;
+	Die die2;
+	Shooter shooter;
+	die1.roll();
+	die2.roll();
+	Roll* roll = shooter.shoot(die1, die2);
+	PointPhase point_phase(roll->roll_value());
+	RollOutcome roll_outcome = point_phase.get_outcome(roll, roll->roll_value());
+	if (roll_outcome.get_value() == RollOutcome::Value::point)
+	{
+		REQUIRE(roll_outcome.get_value() == RollOutcome::Value::point);
+	}
+	else if (roll_outcome.get_value() == RollOutcome::Value::seven_out)
+	{
+		REQUIRE(roll_outcome.get_value() == RollOutcome::Value::seven_out);
+	}
+	else
+	{
+		REQUIRE(roll_outcome.get_value() == RollOutcome::Value::nopoint);
+	}
 }
